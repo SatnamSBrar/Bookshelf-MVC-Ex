713c409e9193293c204bc3cc7c78303d1ba16a68
Satnam Brar <satnamsbrar@gmail.com>
2017-02-06 14:10:32 -0500

    added classes book and category, added bookscontroller

--- Bookshelf/Models/Book.cs
+++ Bookshelf/Models/Book.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+
 
 namespace Bookshelf.Models
 {
     public class Book
     {
+        [Key]   //data annotation
+        public int ID { get; set; } //key is the ID
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")]    //creates another key to organize records
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
